b9c918d121ab8ca98757da2416b8924a40ab5931
Suresh Khatri <38423131+shreshkhatri@users.noreply.github.com>
2025-02-25 12:41:50 +0000

    Update portfolio.tsx

--- src/app/lib/assets/data/portfolio.tsx
+++ src/app/lib/assets/data/portfolio.tsx
@@ -5,13 +5,18 @@ export const portfolioArray = [
       description: "Creating theCV.org App was a hands-on journey of design and development. From conceptualization to implementation, I crafted an intuitive platform where users can effortlessly build, save, and download their professional CVs. Using technologies like NextJS, React, and Node.js, I ensured a seamless experience. The app reflects my commitment to user-friendly design and a passion for empowering individuals in their career pursuits. "
     },
     {
-      src: "https://hccs.gaasnetwork.org",
-      title: "HCCS Conference 2023 (https://hccs.gaasnetwork.org )",
+      src: "https://hccsconf.org/2022/",
+      title: "HCCS Conference 2023 (https://hccsconf.org/2022/ )",
       description: "A Flask application for facilitating  the annual international conference on Human-centred Cognitive Systems (HCCS) is a flagship conference to drive innovation for future industry 5.0 application scenarios. The HCCS calls for original papers and proposals to cover a variety of Human-centred Cognitive Systems stretched across city infrastructures, manufacturing, retail, business, tourism, health, law, and education."
     },
     {
       src: "https://chat-ui-for-ai.vercel.app",
       title: "Chat GPT like UI",
       description: "A NextJS application similar to ChatGPT user interface. UI can be added to any chat service using streaming using numerous AI SDKs, e.g. HuggingFace, LangChain and OpenAI models."
+    },
+    {
+      src: "https://unilms.click",
+      title: "Moodle based LMS ",
+      description: "A Moodle based LMS which allows course and user management. The LMS can be used at conultancies and educational institutions"
     }
 ];
