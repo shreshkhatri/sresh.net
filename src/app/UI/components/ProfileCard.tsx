9a9c7db54db5f77950c393a55c69e4b81dd895d3
Suresh Khatri <38423131+shreshkhatri@users.noreply.github.com>
2025-02-27 05:17:39 +0000

    Update ProfileCard.tsx

--- src/app/UI/components/ProfileCard.tsx
+++ src/app/UI/components/ProfileCard.tsx
@@ -17,9 +17,9 @@ export default function ProfileCard() {
             <CardContent sx={{ alignItems: 'center', textAlign: 'center' }}>
                 <Avatar src="/images/SureshProfilePic.jpeg" sx={{ '--Avatar-size': '10rem',border:'3px solid white' }} />
                 <Typography level="title-lg">Suresh Khatri</Typography>
-                <Typography level="body-sm" >You could write me on any of the following email addresses</Typography>
+                <Typography level="body-sm" >You could write me on the following email addresses</Typography>
                 <Typography level="body-sm" fontWeight={'bold'}>
-                    khatris4@lsbu.ac.uk or shresh.khatri@gmail.com
+                    shresh.khatri@gmail.com
                 </Typography>
                 <br></br>
                 <br></br>
